7cb06994a3bab2a3dc070dd2dab68f9ba132a435
Виталий Манский <workitforme@mail.ru>
2025-10-30 12:55:58 +0300

    My changes

--- api/Books.py
+++ api/Books.py
@@ -10,15 +10,23 @@ from sqlalchemy import select
 router_books = APIRouter(prefix='/books', tags=['Books'])
 
 @router_books.post("/", response_model=BookResponse, status_code=201)
-async def create_book(book: BookCreate, db: AsyncSession = Depends(get_db)):
+async def add_book(book: BookCreate, db: AsyncSession = Depends(get_db)):
     db_book = Book(**book.model_dump())
     db.add(db_book)
     await db.commit()
     await db.refresh(db_book)
     return db_book
 
+@router_books.get('/{book_id}', response_model=BookResponse)
+async def get_book(book_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Book).where(Book.book_id == book_id))
+    book = result.scalar_one_or_none()
+    if book is None:
+        raise HTTPException(status_code=404, detail='Book not found')
+    return book
+
 @router_books.get("/", response_model=List[BookResponse])
-async def read_books(db: AsyncSession = Depends(get_db)):
+async def get_books(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Book))
     books = result.scalars().all()
     return books
--- main.py
+++ main.py
@@ -8,9 +8,5 @@ app = FastAPI(title='Book API')
 
 app.include_router(router_books)
 
-
-
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000)
